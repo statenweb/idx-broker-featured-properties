10bf5e166749ce70e012aa9773387aca7c880c45
Gargano, Matthew <mgargano@penguinrandomhouse.com>
2018-01-09 10:11:22 -0500

    clean up the API request, remove unnecessary old code, up cache length to 10 minutes

--- idx-broker-featured-properties/properties/request.php
+++ idx-broker-featured-properties/properties/request.php
@@ -6,9 +6,8 @@ use IDX_Broker_Featured_Properties\Settings\Global_Settings;
 
 class Request {
 
-	const OPTION_NAME_FOR_SUPPLEMENTAL_DATA = 'properties_plaza_supplemental';
 	const TRANSIENT_KEY = 'ibfp_properties_from_api';
-	const TRANSIENT_LENGTH = 60;
+	const TRANSIENT_LENGTH = 600;
 
 	public static function get_properties() {
 
@@ -19,8 +18,6 @@ class Request {
 
 		$data = get_transient( self::TRANSIENT_KEY );
 		if ( ! $data ) {
-
-
 			$idx_api               = new \IdxBrokerAPI( $api_key );
 			$supplemental_listings = $idx_api->build_request( 'clients/supplemental' )->request();
 			$featured_listings     = $idx_api->build_request( 'clients/featured' )->request();
@@ -34,19 +31,10 @@ class Request {
 	}
 
 
-	private static function get_supplemental_data() {
-		return get_option( self::OPTION_NAME_FOR_SUPPLEMENTAL_DATA );
-	}
-
-
 	private static function rekey_data( $data ) {
 		$rekeyed_data = [];
-		$supplemental = self::get_supplemental_data();
 		foreach ( $data as $datum ) {
-			$new_key = $datum['idxID'] . $datum['listingID'];
-			if ( array_key_exists( $new_key, (array) $supplemental ) ) {
-				$datum = array_merge( $datum, $supplemental[ $new_key ] );
-			}
+			$new_key                  = $datum['idxID'] . $datum['listingID'];
 			$rekeyed_data[ $new_key ] = $datum;
 		}
 
